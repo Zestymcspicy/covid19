8c1751b4113daeb6b7680ac5562f39b2667bc565
Zestymcspicy <halbeckerman@yahoo.com>
2020-03-29 19:51:38 -0500

    presenting data, erasing state date when selecting county

--- script.js
+++ script.js
@@ -1,45 +1,119 @@
-let stateData, countyData, parsedStates;
+let stateData, countyData, parsedStates, currentState, currentCounty;
 const getData = type => {
   return fetch(
-    `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-${type}.csv`,
-    {
+    `https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-${type}.csv`, {
       method: 'GET',
     }
-).then(response => response.text()
-).then(data=> parseCSV(data, type))
+  ).then(response => response.text()).then(data => parseCSV(data, type))
 };
 
 const getCountyData = () => {
   return fetch(
-    "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
-    {
+    "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", {
       method: 'GET',
     }
-).then(response => response.text()
-).then(data=> parseCSV(data, "county"))
+  ).then(response => response.text()).then(data => parseCSV(data, "county"))
 };
 
 const parseCSV = (csv, type) => {
-  csv = csv.split("\n").map(x=>x.split(","))
-  let legend=csv.shift();
+  csv = csv.split("\n").map(x => x.split(","))
+  let legend = csv.shift();
   csv = csv.map(point => {
-    let obj={};
+    let obj = {};
     legend.forEach((prop, index) => {
-      obj[prop]=point[index];
+      obj[prop] = point[index];
     })
     return obj;
   })
-  if(type==="states"){
-    stateData=csv;
+  if (type === "states") {
+    stateData = csv;
     parseStates(stateData);
-  }  else {
-    countyData=csv;
+    getData("counties");
+  } else {
+    countyData = csv;
+    addCounties()
   }
 }
 
-const parseStates = (obj) => {
-
+const parseStates = (arr) => {
+  parsedStates = {};
+  arr.forEach(point => {
+    if (parsedStates[point.state] === undefined) {
+      parsedStates[point.state] = {
+        byDate: []
+      }
+      parsedStates[point.state].byCounty = {}
+    }
+    parsedStates[point.state].byDate.push(point);
+  })
+  console.log(parsedStates)
 }
 
+const addCounties = () => {
+  countyData.forEach(point => {
+    if(parsedStates[point.state]===undefined){
+      parsedStates[point.state] = {
+        byCounty : {}
+      }
+    }
+    if (parsedStates[point.state].byCounty[point.county] === undefined) {
+      parsedStates[point.state].byCounty[point.county] = [];
+    }
+    parsedStates[point.state].byCounty[point.county].push(point);
+  })
+  populateDropdown(parsedStates, "states")
+}
+
+const selectState = e => {
+  currentState = parsedStates[e.target.value].byCounty
+  console.log(currentState)
+  addData(parsedStates[e.target.value].byDate, "state")
+  populateDropdown(currentState, "counties")
+}
+
+const selectCounty = e => {
+  currentCounty = currentState[e.target.value]
+  // console.log(currentCounty)
+  addData(currentCounty, "county")
+}
+
+const addData = (data, type)=> {
+  let dataContainer;
+  if(type="county"){
+    dataContainer = document.getElementById("countyDataContainer");
+  } else {
+    dataContainer = document.getElementById("stateDataContainer")
+  }
+  while (dataContainer.lastChild){
+    dataContainer.removeChild(dataContainer.lastChild)
+  }
+  data.forEach(item=> {
+    dataContainer.insertAdjacentHTML("beforeend", `<p>Date: ${item.date} Cases: ${item.cases}<p>`)
+  })
+}
+
+const populateDropdown = (obj, type) => {
+  let dropdown;
+  if(type==="states"){
+    dropdown = document.getElementById("stateInput")
+    dropdown.addEventListener("change", e => selectState(e))
+  }else{
+    dropdown = document.getElementById("countyInput")
+    dropdown.addEventListener("change", e=> selectCounty(e))
+    while (dropdown.lastChild){
+      dropdown.removeChild(dropdown.lastChild)
+    }
+  }
+  let arr = []
+  for(x in obj){
+    arr.push(x);
+  }
+  arr=arr.sort()
+  arr.forEach(item => {
+    dropdown.insertAdjacentHTML("beforeend", `<option class="state-option" value=${item}>${item}</option>`)
+  })
+}
+
+
+
 getData("states");
-getData("counties");
